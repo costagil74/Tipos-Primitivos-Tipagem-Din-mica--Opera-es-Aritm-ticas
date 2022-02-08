8b11c3c13f57b52c97de981414f734c2ad91b49d
costagil74 <eusoude74@gmail.com>
2022-02-08 18:32:18 -0300

    Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas

---
+++ script.js
@@ -0,0 +1,21 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {firstName: 'Ana', lastName: 'Santos',};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo)
+console.log(typeof patientEmail)
+console.log(patientInfo.firstName)
+console.log(patientInfo.lastName)
+
+const base = 5;
+let height = 8;
+const area = base * height;
+
+console.log(area);
+
+const perimeter = base + height + area;
+console.log(perimeter);
+
